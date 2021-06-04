b12d7ee63755c97ba3202447a91f4117291c0580
Osmany Pujol <Osmany.Pujolcs@gmail.com>
2021-06-03 22:02:27 -0400

    finished the database class

--- src/com/Banking/BankAccount.java
+++ src/com/Banking/BankAccount.java
@@ -1,7 +1,32 @@
 package com.Banking;
 
+import javax.swing.*;
+
 public class BankAccount {
     private double balance;
+    private Customer customer;
 
+    public BankAccount(Customer customer, double balance){
+        this.balance = balance;
+        this.customer = customer;
+    }
 
+    public double getBalance(){
+        return this.balance;
+    }
+
+    public void deposit(double desposit){
+        this.balance += desposit;
+    }
+
+    public void withdrawal(double withdrawal){
+        if(this.balance<withdrawal){
+            JOptionPane.showMessageDialog(null, "Sorry but your withdrawal exceeds your " +
+                    "balance of $"+getBalance());
+        }else {
+            this.balance -= withdrawal;
+            JOptionPane.showMessageDialog(null, "Your withdrawal was successful " +
+                    "your new balance is $"+getBalance());
+        }
+    }
 }
--- src/com/Banking/Database.java
+++ src/com/Banking/Database.java
@@ -1,4 +1,68 @@
 package com.Banking;
 
+import java.util.ArrayList;
+
 public class Database {
+    private ArrayList<Customer> list;
+    private Customer customer;
+
+    private int index;
+    private boolean found;
+
+    //Database Constructor
+    public Database(){
+        list = new ArrayList<Customer>();
+        customer = null;
+        index = 0;
+        found = false;
+    }
+
+    //search for a customer
+    public void search(String account){
+        found = false;
+
+        int i = 0;
+        while (!found && i< list.size()){
+
+            Customer c = list.get(i);
+            if(c.getAccountNumber().equalsIgnoreCase(account)) {
+                customer = c;
+                found = true;
+                index = i;
+            }else{
+                i++;
+            }
+        }
+    }
+
+    public void add(Customer newCustomer){
+        list.add(newCustomer);
+    }
+
+    public int getIndex(){
+        return this.index;
+    }
+
+    public boolean inList(){
+        return found;
+    }
+
+    public int size(){
+        return list.size();
+    }
+
+    public Customer delete(int i){
+        return  list.remove(i);
+    }
+
+    public boolean isEmpty()
+    {
+        return list.isEmpty();
+    }
+
+    public ArrayList<Customer> getList()
+    {
+        return list;
+    }
+    
 }
