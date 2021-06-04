03a70f061a08ea4eb562ed6c53827c30e671309a
Osmany Pujol <Osmany.Pujolcs@gmail.com>
2021-06-03 21:33:13 -0400

    created the Customer class

--- src/com/Banking/Customer.java
+++ src/com/Banking/Customer.java
@@ -9,4 +9,36 @@ public class Customer {
         this.lastname = name;
         this.age = age;
     }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
