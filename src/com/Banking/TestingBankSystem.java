175a448b41503a9dd0d569776508a759d80237f0
Osmany Pujol <Osmany.Pujolcs@gmail.com>
2021-06-03 22:55:02 -0400

    working on TestingBankSystem

--- src/com/Banking/TestingBankSystem.java
+++ src/com/Banking/TestingBankSystem.java
@@ -11,6 +11,24 @@ public class TestingBankSystem {
         //We need to get the information from the person
         // A person can make a withdraw and a deposit
 
+        int subMenu =0;
+        boolean isDone = false;
 
+        // loop until, the program is done
+        while(!isDone){
+            // prompt the user for the menu
+            int selectedMenu = GetData.getInt(
+                    "Welcome to XBank\n\t1. Create account\n"
+                            + "\t2. Delete account\n\t3. Deposit\n"
+                            + "\t4. Withdrawal\n\t5. Customer Log\n"
+                            + "\t6. List of deleted accounts\n\t7. Quit");
+
+            switch (selectedMenu){
+
+                case 1:
+                    
+            }
+
+        }
     }
 }
